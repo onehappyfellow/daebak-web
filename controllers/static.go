3d1eb7ba43b50a142c1965701aa6455fccc7e38e
Jonathan Droege <jonathan.droege@gmail.com>
2024-11-18 17:33:46 -0700

    basic boilerplate

---
+++ controllers/static.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/onehappyfellow/daebak-web/views"
+)
+
+func StaticHandler(tpl views.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+}
