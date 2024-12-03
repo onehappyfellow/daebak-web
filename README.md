022417f559ba8f8c2232bd8cd76083d62843b743
Jonathan Droege <jonathan.droege@gmail.com>
2024-12-02 23:22:13 -0700

    article single and list

---
+++ README.md
@@ -0,0 +1,7 @@
+# Daebak Korean Backend
+
+## Development
+- docker-compose up
+- docker exec -it daebak-web_db_1 psql -U onehappyfellow -d daebak
+- create table if not exists
+- go run main.go
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/onehappyfellow/daebak-web/controllers"
+	"github.com/onehappyfellow/daebak-web/models"
 	"github.com/onehappyfellow/daebak-web/templates"
 	"github.com/onehappyfellow/daebak-web/views"
 )
@@ -16,6 +17,30 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// setup the database
+	db, err := models.Open(models.DefaultPostresConfig())
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// setup services
+	articleService := &models.ArticleService{
+		DB: db,
+	}
+
+	// controllers
+	articleController := controllers.Articles{
+		ArticleService: articleService,
+	}
+	articleController.Templates.Single = views.Must(views.ParseFS(
+		templates.FS, "layout.gohtml", "article.gohtml",
+	))
+	articleController.Templates.List = views.Must(views.ParseFS(
+		templates.FS, "layout.gohtml", "article-list.gohtml",
+	))
+
+	// setup router
 	r := chi.NewRouter()
 	r.Use(middleware.StripSlashes)
 	r.NotFound(notFoundHandler)
@@ -27,9 +52,11 @@ func main() {
 	r.Get("/contact", controllers.StaticHandler(
 		views.Must(views.ParseFS(templates.FS, "layout.gohtml", "contact.gohtml")),
 	))
+	r.Get("/a", articleController.Single)
+	r.Get("/trending", articleController.Trending)
 
 	fmt.Println("Starting server on port 3000")
-	err := http.ListenAndServe(":3000", r)
+	err = http.ListenAndServe(":3000", r)
 	if err != nil {
 		fmt.Println(err)
 	}
